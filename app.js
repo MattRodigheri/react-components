c810969ac2d2826769fd931ba3125da219237ec9
MattRodigheri <mrodigheri@gmail.com>
2018-08-14 22:13:37 -0700

    bold on hover

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
-// TODO
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hovering: false
+    };
+  }
+
+  mouseOver() {
+    this.setState({
+      hovering: true
+    });
+  }
+
+  mouseOut() {
+    this.setState({
+      hovering: false
+    });
+  }
+
+  render() {
+
+    var style = {
+     fontWeight: this.state.hovering ? 'bold' : 'normal'
+   };
+
+    return (
+      <li style={style} onMouseOver={this.mouseOver.bind(this)} onMouseOut={this.mouseOut.bind(this)}>{this.props.item}</li>
+    );
+  }
+}
+
+var GroceryList = (props) => (
+  <ul>
+    {props.groceryItems.map(item =>
+      <GroceryListItem item={item} />
+    )}
+  </ul>
+);
+
+var App = () => (
+  <div>
+    <GroceryList groceryItems={['Milk', 'Eggs']}/>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
